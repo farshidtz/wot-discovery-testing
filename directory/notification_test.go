ea2721d1e1fc60513f7df6b1beff7fa6ba468d38
Farshid Tavakolizadeh <email@farshid.ws>
2021-06-18 10:56:36 +0200

    add assertions to tests

--- directory/notification_test.go
+++ directory/notification_test.go
@@ -4,28 +4,45 @@ import (
 	"testing"
 )
 
-// RFC2119 Assertions IDs:
-// tdd-notification-event-types
-// tdd-notification-filter-type
-// tdd-notification-data
-// tdd-notification-data-tdid
-// tdd-notification-data-create-full
-// tdd-notification-data-update-diff
-// tdd-notification-data-update-id
-// tdd-notification-data-delete-diff
-// tdd-notification-data-diff-unsupported
-
 func TestCreateEvent(t *testing.T) {
-	defer report(t, &record{comments: "TODO"})
-	t.SkipNow()
+	defer report(t, &record{assertions: []string{
+		"tdd-notification-sse",
+		"tdd-notification-event-id",
+		"tdd-notification-event-types",
+		"tdd-notification-filter-type",
+		"tdd-notification-data",
+		"tdd-notification-data-tdid",
+		"tdd-notification-data-create-full",
+		"tdd-notification-data-diff-unsupported",
+	}})
+	t.Skip("TODO")
 }
 
 func TestUpdateEvent(t *testing.T) {
-	defer report(t, &record{comments: "TODO"})
-	t.SkipNow()
+	defer report(t, &record{assertions: []string{
+		"tdd-notification-sse",
+		"tdd-notification-event-id",
+		"tdd-notification-event-types",
+		"tdd-notification-filter-type",
+		"tdd-notification-data",
+		"tdd-notification-data-tdid",
+		"tdd-notification-data-update-diff",
+		"tdd-notification-data-update-id",
+		"tdd-notification-data-diff-unsupported",
+	}})
+	t.Skip("TODO")
 }
 
 func TestDeleteEvent(t *testing.T) {
-	defer report(t, &record{comments: "TODO"})
-	t.SkipNow()
+	defer report(t, &record{assertions: []string{
+		"tdd-notification-sse",
+		"tdd-notification-event-id",
+		"tdd-notification-event-types",
+		"tdd-notification-filter-type",
+		"tdd-notification-data",
+		"tdd-notification-data-tdid",
+		"tdd-notification-data-delete-diff",
+		"tdd-notification-data-diff-unsupported",
+	}})
+	t.Skip("TODO")
 }
