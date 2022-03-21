bd2cce93579c2eeee3c76ca2d402db305cfa8656
Farshid Tavakolizadeh <email@farshid.ws>
2022-03-21 16:37:20 +0100

    Remove local assertions list

---
+++ directory/constants.go
@@ -0,0 +1,8 @@
+package directory
+
+const (
+	MediaTypeJSON             = "application/json"
+	MediaTypeJSONLD           = "application/ld+json"
+	MediaTypeThingDescription = "application/td+json"
+	MediaTypeMergePatch       = "application/merge-patch+json"
+)
